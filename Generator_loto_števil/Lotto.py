c5e3e64f41fbfe1afeae647f6fa8ad373b75c8b7
Damir Tešnjak <damir.tesnjak@studioproteus.si>
2018-03-07 16:54:59 +0100

    no message

---
+++ Generator_loto_števil/Lotto.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+
+'''Author: Damir Tešnjak'''
+
+import random
+
+print "\n----Lottery numbers generator----"
+
+def inputNum():
+
+    try:
+        global x
+        x = int(raw_input('\nPlease enter how many random numbers would\n'
+                          'you like to have (1-39): '))
+
+    except ValueError:
+        print("Input is not a number! Try again! ")
+        inputNum()
+
+    return generator(x)
+
+def generator(num):
+
+    numList = []
+    count = 0
+
+    while count < num:                      # Število ižrebanih številk zagotovimo s štetjem vnesenih številk v seznam.
+        randNum = random.randint(1, 39)     # Nakljućno ižrebana številka (1-39)
+        if randNum not in numList:
+            numList.append(randNum)
+            count += 1
+
+    numList.sort()                          # Številke razvrstimo od najmanjše do največje
+    return numList
+
+print "\n", inputNum(), "\nEND"             #Izpis
