63a7deccf254f82d6ea345bc348ec37ea55fe08e
Damir Tešnjak <damir.tesnjak@studioproteus.si>
2018-03-13 17:20:45 +0100

    prva verzija

---
+++ Citati/Citati.py
@@ -0,0 +1,18 @@
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
+import random
+
+url = "http://quotes.yourdictionary.com/theme/marriage/"
+response = urlopen(url).read()
+soup = BeautifulSoup(response)
+
+quotes = []
+
+for quote in soup.findAll("p", attrs={"class": "quoteContent"}):
+    quotes.append(quote.string)
+
+step = 0
+
+while step < 5:
+    print quotes[random.randint(0, len(quotes) - 1)]
+    step += 1
