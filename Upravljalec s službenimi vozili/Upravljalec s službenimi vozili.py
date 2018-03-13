5fd8c4a8facf7ffcb1430d49e30bdca32d78f3b4
Damir Tešnjak <damir.tesnjak@studioproteus.si>
2018-03-13 04:22:05 +0100

    UPRAVLJALEC VOZIL DRUGA VERZIJA

--- Upravljalec s službenimi vozili/Upravljalec s službenimi vozili.py
+++ Upravljalec s službenimi vozili/Upravljalec s službenimi vozili.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-''' UPRAVLJALEC VOZNEGA PARKA
-    Author: Damir Tešnjak
-	Python 3.x'''
+''' Ugani število
+    Author: Damir Tešnjak'''
 
-print ("\n----Upravljalec voznega parka----\n")
+print "\n----Upravljalec voznega parka----\n"
 
 
 # POMOČ UPORABNIKU
 
 def help():
-    print ("Ukaz  Opis" \
-          "\n\nS:    Seznam vozil" \
-          "\nKM:   Ševilo prevoženih kilometrov" \
-          "\nSER:  Datum zadnjega servisa" \
-          "\nV:    Novo vozilo" \
-          "\nO:    Odstrani vozilo iz seznama" \
-          "\nI:    Izhod iz posamezne opcije" \
-          "\nEND:  Izhod iz programa" \
-          "\nP:    Pomoč")
-
+    print "______________________________________________"\
+          "\n| Ukaz  | Opis                               |" \
+          "\n----------------------------------------------" \
+          "\n| S:    | Seznam vozil                       |" \
+          "\n| KM:   | Ševilo prevoženih kilometrov       |" \
+          "\n| SER:  | Datum zadnjega servisa             |" \
+          "\n| V:    | Novo vozilo                        |" \
+          "\n| O:    | Odstrani vozilo iz seznama         |" \
+          "\n| I:    | Kadarkoli izhod iz posamezne opcije|" \
+          "\n| END:  | Izhod iz programa                  |" \
+          "\n| P:    | Pomoč                              |" \
+          "\n----------------------------------------------"
+    print "Če želite predčasno končati program, najprej" \
+          "\nzapustite opcijo (I), šele nato končajte" \
+          "\nprogram (END). Nepravilna uporaba ukazov in" \
+          "\nnapačnih vnosov lahko 'podre' program in" \
+          "\nPOVZROČI IZGUBO PODATKOV!"
 
 # S CLASSOM USTVARJENO VOZILO
 
@@ -42,38 +48,58 @@ def new_vehicle( brand, model, km, service, id_num ):
     
     vehicle = object_Vehicle( brand, model, km, service )
 
-    data_to_write = id_num + " Znamka: " + vehicle.brand + " Model: " + vehicle.model + " Prevoženi kilometri: " + \
-                    vehicle.km + " km Datum servisa: " + vehicle.service
+    data_to_write = id_num + "," + \
+                    vehicle.brand + "," + \
+                    vehicle.model + "," + \
+                    vehicle.km + "," + \
+                    vehicle.service + ","
     writing_into_file( data_to_write )
 
 
 # BRANJE DATOTEKE, OGLED SEZNAMA
 
 def vehicle_list():
-    print ("\n----SEZNAM VOZIL----\n")
+
+    print "\n----SEZNAM VOZIL----\n"
+    print "_________________________________________________________________"
+    print "| ID  | ZNAMKA     | MODEL      | KM         | DATUM SERVISA    |"
+    print "-----------------------------------------------------------------"
+
     with open( "data_vozila.txt", "r+" ) as file:
         lines = file.readlines()
         lines.sort()
+
         if len( lines ) == 0:
-            print ("Seznam vozil je prazen!")
+            print "Seznam vozil je prazen!\n"
+            return False
         else:
             for line in lines:
-                print (line)
-    print ("\n--------------------\n")
+                try:
+                    lineAsList = line.split(",")
+                    print "| " + lineAsList[0] + " | " + lineAsList[1] + " " * (10 - len(lineAsList[1])) + \
+                          " | " + lineAsList[2] + " " * (10 - len(lineAsList[2])) + \
+                          " | " + lineAsList[3] + " " * (10 - len(lineAsList[3])) + \
+                          " | " + lineAsList[4] + " " * (17 - len(lineAsList[4])) + "|"
+                except IndexError:
+                    continue
+    print "-----------------------------------------------------------------\n"
 
 
 # PISANJE V DATOTEKO
 
 def writing_into_file( new_line ):
+
     with open( "data_vozila.txt", "a" ) as file:
-        file.write( new_line + "\n" )
+        file.write( new_line + "\n")
 
 
 # ISKANJE VOZILA
 
 def find_vehicle( id_num ):
+
     with open( "data_vozila.txt", "r" ) as file:
         lines = file.readlines()
+
         for line in lines:
             if id_num in line:
                 return line
@@ -82,114 +108,177 @@ def find_vehicle( id_num ):
 # BRISANJE IZ DATOTEKE
 
 def delete_vehicle( id_num ):  # Koda iz Stockoverflow
+
     try:
         with open( "data_vozila.txt", "r+" ) as f:
             t = f.read()
             to_delete = find_vehicle( id_num ).strip()  # kaj izbrišemo
             f.seek( 0 )
-            for line in t.split( '\n' )[ :-1]:
+
+            for line in t.split( "\n" ):
                 if line != to_delete:
-                    f.write( line + '\n' )
+                    f.write( line + "\n" )
             f.truncate()
+
     except AttributeError:
-        print ("Vozilo v bazi podatkov ne obstaja!\nPoskusite znova!")
+
+        print "Vozilo v bazi podatkov ne obstaja!" \
+              "\nPoskusite znova!"
 
 
-# SKUPNI DEFINICIJI
+def remove_empty_lines(): # Koda iz Stockoverflow
+    """Overwrite the file, removing empty lines and lines that contain only whitespace."""
+    with open("data_vozila.txt", 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.writelines(line for line in lines if line.strip())
+        f.truncate()
+
+
+# SKUPNI DEFINICIJI PRI UREJANJU PODATKOV
 
 def shared_def():
-    vehicle_list()
-    id_num = input( "Vnesite ID številko vozila: " )
-    selected_Vehicle = find_vehicle( id_num ).split()
-    delete_vehicle( id_num )
-    return selected_Vehicle
+
+    if vehicle_list() == False:
+        command()
+    else:
+        id_num = raw_input( "Vnesite ID številko vozila: " )
+
+        if id_num.upper() == "I":
+            return False
+        else:
+            selected_Vehicle = find_vehicle( id_num ).split(",")
+            delete_vehicle( id_num )
+            return selected_Vehicle
 
 
 def shared_def2( selected_Vehicle ):
-    new_line = ' '.join( selected_Vehicle )
+
+    new_line = ','.join( selected_Vehicle )
     writing_into_file( new_line )
-    vehicle_list()
 
 
 # UREJANJE ŠT. KILOMETROV
 
 def change_km():
+
     selected_Vehicle = shared_def()
-    kilometers = input( "Vnesite novo število kilometrov: " )
-    selected_Vehicle.insert( 7, kilometers )
-    del selected_Vehicle[ 8 ]
-    shared_def2( selected_Vehicle )
+    if selected_Vehicle == False:
+        return False
+    else:
+        kilometers = raw_input( "Vnesite novo število kilometrov: " )
+
+        if kilometers.upper() == "I":
+            return False
+        else:
+            selected_Vehicle.insert( 3, kilometers )
+            del selected_Vehicle[ 4 ]
+            shared_def2( selected_Vehicle )
 
 
 # UREJANJE DATUMA ZADNJEGA SERVISA
 
 def change_date_service():
+
     selected_Vehicle = shared_def()
-    service = input( "Vnesite nov datum zadnjega servisa ( dd-mm-yyyy ): " )
-    selected_Vehicle.insert( 11, service )
-    del selected_Vehicle[ 12 ]
-    shared_def2( selected_Vehicle )
+
+    if selected_Vehicle == False:
+        return False
+    else:
+        service = raw_input( "Vnesite nov datum zadnjega servisa ( dd-mm-yyyy ): " )
+
+        if service.upper() == "I":
+            return False
+        else:
+            selected_Vehicle.insert( 4, service )
+            del selected_Vehicle[ 5 ]
+            shared_def2( selected_Vehicle )
 
 
 # VNOS PODATKOV VOZILA
 
 def input_vehicle():
     while True:
-        brand = input( "\nZnamka vozila: " )
+        brand = raw_input( "\nZnamka vozila: " )
 
         if brand.upper() == "I":
             break
         else:
-            model = input( "Model vozila: " )
+            model = raw_input( "Model vozila: " )
             if model.upper() == "I":
                 break
             else:
-                km = input( "Število prevoženih kilometrov (brez 'km'): " )
+                km = raw_input( "Število prevoženih kilometrov: " )
                 if km.upper() == "I":
                     break
                 else:
-                    service = input( "Datum zadnjega servisa ( dd-mm-yyyy ): " )
+                    service = raw_input( "Datum zadnjega servisa ( dd-mm-yyyy ): " )
                     if service.upper() == "I":
                         break
                     else:
-                        id_num = input( "Vnesite ID številko ( primer: 001, 002, 003... ). " )
+                        id_num = raw_input( "Vnesite ID številko ( primer: 001, 002, 003... ). " )
                         if id_num.upper() == "I":
                             break
                         else:
                             try:
                                 if find_vehicle( id_num ).split( " " )[ 0]   == id_num:
-                                    print ("Vozilo z ID " + id_num + " že obstaja. Poskusite znova.")
+                                    print "Vozilo z ID " + id_num + " že obstaja. Poskusite znova."
                                     input_vehicle()
                             except AttributeError:
                                 new_vehicle( brand, model, km, service, id_num )
-                                vehicle_list()
 
 
 # IZBIRA UPORABNIKA
 
 def command():
     while True:
-        option = input( "\nIzberite opcijo: " )
+
+        option = raw_input( "\nIzberite opcijo. " )
+
         if option.upper() == "S":
             vehicle_list()
+
         elif option.upper() == "KM":
-            change_km()
+            if change_km() == False:
+                continue
+            else:
+                change_km()
+                remove_empty_lines()
+
         elif option.upper() == "SER":
-            change_date_service()
+            if change_date_service() == False:
+                continue
+            else:
+                change_date_service()
+                remove_empty_lines()
+                vehicle_list()
+
         elif option.upper() == "V":
             vehicle_list()
             input_vehicle()
             vehicle_list()
+
         elif option.upper() == "O":
             vehicle_list()
-            id_num = input( "Vnesite ID številko ( primer: 001, 002, 003... ). " )
-            delete_vehicle( id_num )
-            print ("Vozilo je izbrisano")
-            vehicle_list()
+            id_num = raw_input( "Vnesite ID številko ( primer: 001, 002, 003... ). " )
+
+            if id_num.upper() == "I":
+                continue
+            else:
+                warning = raw_input("Potrdtitev izbrisa (D/N). ")
+                if warning.upper() == "D":
+                    delete_vehicle(id_num)
+                    remove_empty_lines()
+                    vehicle_list()
+                else:
+                    continue
+
         elif option.upper() == "P":
             help()
+
         elif option.upper() == "END":
-            print ("Konec programa")
+            print "----------------" \
+                  "\nKonec programa."
             break
 
 help()
