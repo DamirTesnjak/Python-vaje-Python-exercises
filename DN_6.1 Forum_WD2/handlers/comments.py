ff4c90e42b4a49828d192aa401d0a496547de9a8
Damir Tešnjak <damir.tesnjak@studioproteus.si>
2018-07-09 10:45:38 +0200

    no message

---
+++ DN_6.1 Forum_WD2/handlers/comments.py
@@ -0,0 +1,26 @@
+from handlers.base import BaseHandler
+from google.appengine.api import users
+from models.models import Topic, Comment
+from google.appengine.api import memcache
+import uuid
+
+class AddComment(BaseHandler):
+    def post(self, topic_id):
+        csrf_token = self.request.get("csrf_token")
+        mem_token = memcache.get(key=csrf_token)  # find if this CSRF exists in memcache
+
+        if not mem_token:  # if token does not exist in memcache, write the following message
+            return self.write("Attack attempt detected...")
+
+        user = users.get_current_user()
+
+        if not user:
+            return self.write("Please login before you're allowed to post a topic.")
+
+        topic = Topic.get_by_id(int(topic_id))
+        text = self.request.get("comment")
+
+        comment = Comment(content=text, author_email=user.email(), topic_id=topic.key.id(), topic_title=topic.title)
+        comment.put()
+
+        return self.redirect_to("topic_details", topic_id=topic.key.id())
